52f41f1656502a6ece4a1b2ad32db04840028ae3
melaniegm <s.melanie.gasmen@redclayschools.com>
2017-05-06 17:46:23 -0400

    simplified

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,26 +1,10 @@
 <?php
-if(isset($_POST['Submit'])){
+if($_POST["submit"]){
 $errors = '';
 $myemail = 's.melanie.gasmen@redclayschools.com';//<-----Put Your email address here.
-if(empty($_POST['firstname'])  ||
-   empty($_POST['lastname'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['subject']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-$name = $_POST['firstname.lastname'];
+$name = $_POST['firstname'];
 $email_address = $_POST['email'];
 $message = $_POST['subject'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
 $to = $myemail;
 $email_subject = "Contact form submission: $name";
 $email_body = "You have received a new message. ".
@@ -29,6 +13,6 @@ $email_body = "You have received a new message. ".
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-}
+header("Location:http://cabcallowayschool.org");
 }
 ?>
